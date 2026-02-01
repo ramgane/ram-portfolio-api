615b5f99d3f1e1dd7d93c31d29a6c67709bcd6d5
ramgane <guru4567varan@gmail.com>
2026-02-01 18:04:24 +0530

    healthcheck push

--- server.js
+++ server.js
@@ -13,10 +13,19 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+// Health check
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    message: "API is running fine 🚀",
+    timestamp: new Date().toISOString(),
+  });
+});
+
 // Routes
 app.use("/api/email", emailRoutes);
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
